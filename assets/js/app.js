d1e67594bf75e9e3aa5ed4f47f61a6caa82f53d7
Denis Kuimov <deniskuimov@mail.ru>
2020-06-20 00:46:14 +0500

    Edit link item

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
-$( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val(ui.values[ 0 ] + " -  " + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - " + $( "#slider-range" ).slider( "values", 1 ) );
-  } ); 
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
+
+
+
